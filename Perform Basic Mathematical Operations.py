235e756dcceb35f5fa863c1dd6025a49e97bf7f3
Harsh-kub <harshkubade12345@gmail.com>
2025-05-03 13:33:15 +0530

    Create Perform Basic Mathematical Operations.py

---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,12 @@
+a=input("ENTER THE FIRST NUMBER:")
+b=input("ENTER THE SECOND NUMBER:")
+a=float(a)
+b=float(b)
+q=a+b
+print('the sum of numbers=', q)
+r=a-b
+print('the difference of numbers=', r)
+w=a*b
+print('the product of numbers=', w)
+t=a/b
+print('the quotient of numbers=', t)
